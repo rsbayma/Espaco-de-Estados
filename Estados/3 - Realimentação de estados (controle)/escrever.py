f3187277ee8ef655bcf99f7d2194639523e4ae25
rsbayma <122168383+rsbayma@users.noreply.github.com>
2023-01-18 17:38:58 -0300

    Rev Cap 3

---
+++ Estados/3 - Realimentação de estados (controle)/escrever.py
@@ -0,0 +1,34 @@
+import numpy as np
+#
+def dispmat(A):
+    cols = ''.join(['r']*A.shape[1])
+    str = r'\left[\begin{array}{' + cols + r'}'
+    (m,n) = A.shape
+    for i in range(m):
+      for j in range(n):
+        if j<n-1:
+          str += '{}'.format(A[i,j]) + '&'
+        else:
+          str += '{}'.format(A[i,j]) + r'\\'
+    return str + r'\end{array}\right]'
+
+
+''' def dispss(self,A,B,C,D):
+    str1 = r'\dot{\mathbf{x}} = ' + self.dispmat(A) + r'\mathbf{x}'
+    str1 = str1 + '+' + self.dispmat(B) + r'\mathbf{u}'
+    str2 = r'\mathbf{y} = ' + self.dispmat(C) + r'\mathbf{x}'
+    str2 = str2 + self.dispmat(D) + r'\mathbf{u}'
+    return [str1,str2]
+'''
+
+'''import numpy as np
+n = np.random.randint(4,6)
+p = np.random.randint(1,4)
+q = np.random.randint(1,4)
+
+A = np.random.randn(n,n)
+B = np.random.randn(n,p)
+C = np.random.randn(q,n)
+D = np.random.randn(q,p)
+
+print(dispss(A,B,C,D))'''
